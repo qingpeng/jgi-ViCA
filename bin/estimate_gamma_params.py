01e3502105a020182115661a36fe21626580b96c
Adam R. Rivers <rivers@alum.mit.edu>
2015-07-08 15:55:43 -0700

    Script of estimating gamma parameters from data is complete

--- bin/estimate_gamma_params.py
+++ bin/estimate_gamma_params.py
@@ -11,10 +11,16 @@ import scipy.stats
 from matplotlib import pyplot as plt
 
 
-parser = argparse.ArgumentParser(description='Estimate gamma parameters describing the length of assemblies by sampling')
-parser.add_argument('--input', help="A multi-sequence fasta file",type=argparse.FileType('r'), default='-')
-parser.add_argument('--output', help="output file with the 4 parameters" , type=argparse.FileType('w'), default='-')
-parser.add_argument('--fitdoc', help ="a file with more information on the fit") 
+parser = argparse.ArgumentParser( \
+	description='Estimate gamma parameters describing the length of assemblies by sampling')
+parser.add_argument('--input', help="A multi-sequence fasta file",\
+	type=argparse.FileType('r'), default='-')
+parser.add_argument('--output', help="output file with the 3 parameters" , \
+	type=argparse.FileType('w'), default='-')
+parser.add_argument('--fitplot', help ="a file with more information on the fit", \
+	defualt="gammafit.pdf") 
+parser.add_argument('--plotformat', help ="File format to plot the fit graph", \
+	choices = ["png", "pdf", "ps", "eps" "svg"],defualt="pdf") 
 args = parser.parse_args()
 
 
@@ -24,7 +30,25 @@ for sequence in sequences:
 	slen = len(sequence)
 cnt.append(slen)
 
-#TODO fit with scipi script
-#find min value 
-#subtract min from data and write as offset
-#fit with scipi fit gamma
+# convert length data to a numpy array
+npcnt = np.array(cnt)
+
+#fit gamma parameters with scipi 
+fit_alpha, fit_loc, fit_scale=scipy.stats.gamma.fit(data)
+
+#output the parameters
+args.output.write("alpha","\t","loc","\t","fit_beta","\n")
+args.output.write(fit_alpha,"\t",fit_loc,"\t",fit_beta,"\n")
+
+# Plot the results for manual inspection
+fig, ax = plt.subplots(1, 1)
+# Create curve points for fitted line
+x = np.linspace(scipy.stats.gamma.ppf(0.01, fit_alpha, loc=fit_loc,scale=fit_scale),\
+	scipy.stats.gamma.ppf(0.99, fit_alpha, loc=fit_loc,scale=fit_scale), 100)
+# plot line
+ax.plot(x, scipy.stats.gamma.pdf(x, fit_alpha, loc=fit_loc,scale=fit_scale),'r-', lw=2, \
+	alpha=0.6, label='gamma pdf')
+# plot histogram of data
+ax.hist(npcnt, normed=True, alpha=0.2)
+# Save figure to file
+plt.savefig(args.fitplot, transparent=True, format= args.plotformat)
--- bin/shred.py
+++ bin/shred.py
@@ -12,18 +12,18 @@ parser.add_argument('--input', help="A fasta file",type=argparse.FileType('r'), 
 parser.add_argument('--output', help= "Name of output file written", type=argparse.FileType('w'), default='-')
 parser.add_argument('--samples', help="Total number of shreded contigs to create, or if between 0 and 1, the proportion of the genome to sample", default = 0.5, type=float) 
 parser.add_argument('--length', help="The length of the genome subsamples", default = 100, type=int)
-parser.add_argument('--shape', help="Shape parameter of gamma distribution, k", default =100, type=float)
+parser.add_argument('--alpha', help="Shape parameter of gamma distribution, alpha or k", default =100, type=float)
 parser.add_argument('--scale', help="Scale parameter of gamma distribution, theta", default = 10, type=float)
-parser.add_argument('--offset', help= "Offset, or minimum contig length allowed", default = 0, type=int)
+parser.add_argument('--loc', help= "Offset, or minimum contig length allowed determind from fitting", default = 0, type=int)
 
 args = parser.parse_args()
 
 #Functions
-def estimate_samples_gamma(genomelength, samples, shape, scale, offset):
+def estimate_samples_gamma(genomelength, samples, shape, scale, loc):
     """Return the number of sequences to output from a gamma distribution"""
     assert samples > 0 , "Sample value must be greater than 0"
     if samples <  1:
-        num = genomelength * samples /(shape * scale +offset)
+        num = genomelength * samples /(shape * scale +loc)
     else: 
         num = samples
     return num
@@ -37,7 +37,7 @@ def estimate_samples_fixed(genomelength, samples, length):
         num = samples
     return num
 
-def shred(fasta, shred, samples, shape, scale, offset, length):
+def shred(fasta, shred, samples, shape, scale, loc, length):
     """Take a large FASTA and Return a Multi sequence FASTA with fragments of specified length"""
     #create weighting vectors based on the length of the contigs in the genome
     records = SeqIO.to_dict(SeqIO.parse(fasta, "fasta"))
@@ -54,7 +54,7 @@ def shred(fasta, shred, samples, shape, scale, offset, length):
         weights.append(float(x) / totlen) # Weighting vector for selecting contigs
     #Determine the number of reads to sample
     if shred == "gamma":
-        numsamples = estimate_samples_gamma(sum(slen), samples, shape, scale, offset)
+        numsamples = estimate_samples_gamma(sum(slen), samples, shape, scale, loc)
     if shred == "fixed":
         numsamples = estimate_samples_fixed(sum(slen), samples, length)
     #select the contigs
@@ -65,7 +65,7 @@ def shred(fasta, shred, samples, shape, scale, offset, length):
         assert (attempts < 10 * numsamples), "Too many attempts were made subsampling the genome, adjust the sampling parameters"
         #Calculate sample read length based on gamma distribution
         if shred == "gamma":
-            length = int(np.random.gamma(shape=shape,scale=scale)) + int(offset)
+            length = int(np.random.gamma(shape=shape,scale=scale)) + int(loc)
         print(length)
         id = np.random.choice(a=ids,p=weights)
         selectedseq = records[id]
@@ -84,5 +84,5 @@ def shred(fasta, shred, samples, shape, scale, offset, length):
     return sampled_frags
 
 samples_frags = shred(fasta=args.input, shred=args.shred, samples=args.samples, \
-shape=args.shape, scale=args.scale, offset=args.offset, length=args.length)
+shape=args.shape, scale=args.scale, loc=args.loc, length=args.length)
 SeqIO.write(samples_frags, args.output, "fasta")
