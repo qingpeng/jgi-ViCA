d7af7467fb4f3eb8d4754896c6c04857c3132d9c
Adam R. Rivers <rivers@alum.mit.edu>
2015-07-15 13:13:21 -0700

    Update create_training_data.py to the new RefTree interface

--- bin/create_training_data.py
+++ bin/create_training_data.py
@@ -12,29 +12,30 @@ parser.add_argument('-o', '--output', help ="Reftree database directory location
 args = parser.parse_args()
 
 
+#Variables
+numworkers = str(2)
+
 # Read the configuration file
 config = json.load( open(args.config, 'r'))
 # If an id list is specified in config write it otherwise query everything from the root node \
 if config["trainingid"]:
-	node = config["trainingid"]
+	node = str(config["trainingid"])
 	print("Loading subtree taxon id from config file: %s" % node)
 else:
-	node = 1
+	node = str(1)
 	print("No subtree specified, training on all sequences")
 	
 # Open a subprocess to create a new reftree database directory, have it execute a shell \
 # script that calls single_taxon_training and writes the results to the reftree database directory
 
+# reftreeopts = ["reftree.pl", "--create", taxdmp ]
+# p0 = subprocess.Popen(reftreeopts, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+# reftreeout, reftreeerr= p0.communicate()
+# assert p0.returncode == 0, "RefTree returned the error %s while creating the new database" % reftreeerr
 
-reftreeopts = ["reftree.pl", "--exe", node,"genomic", args.output,\
-	"TRUE", "./single_taxon_training.sh","--","__TAXON__","__OUTFILE__"]
+
+reftreeopts = ["reftree.pl", "--exec", node,"genomic", args.output,\
+	"TRUE",numworkers,"--","single_taxon_training.sh", "__TAXON__","__OUTFILE__"]
 p1 = subprocess.Popen(reftreeopts, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 reftreeout, reftreeerr= p1.communicate()
-assert p1.returncode == 0, "RefTree returned the error %s while executing taxon training" % reftreeerr
-
-
-# Index the reftree database #
-reftree2opts = ["reftree.pl", "--indexDb", args.output]
-p2 = subprocess.Popen(reftree2opts, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-reftree2out, reftree2err= p2.communicate()
-assert p2.returncode == 0, "RefTree returned the error %s while indexing the database" % reftree2err
+assert p1.returncode == 0, "RefTree returned an error while executing taxon training"
--- bin/env.sh
+++ bin/env.sh
@@ -3,5 +3,6 @@ export PATH=$PATH:/global/homes/a/arrivers/dev/jgi-genelearn/bin
 export PYTHONPATH=$PYTHONPATH:/global/homes/a/arrivers/lib/python/python2.7/site-packages/
 module load biopython
 export PATH=/global/homes/e/ekirton/dev/RefTree/perl/bin:$PATH
-export PERL5LIB=/global/homes/e/ekirton/dev/RefTree/perl/lib:$PERL5LIB
+export PERL5LIB=/global/homes/e/ekirton/dev/RefTree/perl/lib:/global/homes/e/ekirton/dev/RefTree/perl/lib/RefTree$PERL5LIB
 export REFTREE_DIR='/global/projectb/scratch/ekirton/RefTree'
+
