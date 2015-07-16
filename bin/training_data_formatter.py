7cf586cd31eafe6f31c2cb7b058217b259e33b1c
Adam R. Rivers <rivers@alum.mit.edu>
2015-07-16 10:10:29 -0700

    renamed kmer extraction, completed my part of training_data_formatter

--- bin/training_data_formatter.py
+++ bin/training_data_formatter.py
@@ -8,22 +8,19 @@ from Bio import SeqIO
 
 parser = argparse.ArgumentParser(description='A script select taxonomic groups from a reftree feature vector database' )
 parser.add_argument('-c', '--config', help ="A json formatted config file")
-parser.add_argument('-o', '--output', help ="An output vector file", )
+parser.add_argument('-o', '--output', help ="An output vector file", type=argparse.FileType('w'), default='-')
 parser.add_argument('-a', '--attribute', help ="Reftree database directory location") 
 args = parser.parse_args()
 
 
 # Functions
-
 def get_reftree_data(dbpath, attributes):
-	"""A function to run reftree on a particular taxonomy level and return the class and vector data"""
-	reftreeopts = ["reftree.pl", "--tree", "dbpath", attributes]
-	p0 = subprocess.Popen(reftreeopts, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-	reftreeout, reftreeerr= p0.communicate()
-	assert p0.returncode == 0, "RefTree returned an error while searching the tree"
-	return reftreeout
-
-
+    """A function to run RefTree on a particular taxonomy level or node and return the attributes and vector data"""
+    reftreeopts = ["reftree.pl", "--tree", "dbpath", attributes]
+    p0 = subprocess.Popen(reftreeopts, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    reftreeout, reftreeerr= p0.communicate()
+    assert p0.returncode == 0, "RefTree returned an error while searching the tree"
+    return reftreeout
 
 # Read the configuration file
 config = json.load( open(args.config, 'r'))
@@ -31,12 +28,22 @@ config = json.load( open(args.config, 'r'))
 
 # If an id list is specified in config write it otherwise query everything from the root node \
 if config["method"] == "level":
-	print("Formatting training data for all taxa at the %s level" % config["levelattr"]["level"]
-	level = config["levelattr"]["level"].lower
-	data = get_reftree_data(config[dbpath],level)
+    print("Formatting training data for all taxa at the %s level" % config["levelattr"]["level"])
+    level = config["levelattr"]["level"].lower
+    data = get_reftree_data(config["dbpath"],level)
+    args.output.write(data)
 elif config["method"] == "taxids":
-	print("Formatting training data for selected taxa")
-	for key, record in Config["Categories"]:
-	TODO
+    print("Formatting training data for selected taxa")
+    for key, record in config["categories"].iteritems():
+        for pair in record:
+            assert pair["taxid"], "A taxon id is required for each category"
+            if "code" in pair:
+                attributes = str(pair["taxid"]) + ":" + str(pair["code"])
+            else:
+                attributes = str(pair["taxid"])
+            #print(str(key) + "\t" + attributes)
+        	data = get_reftree_data(config["dbpath"],attributes)
+        	args.output.write(data)
+        	args.output.write("\n")
 else:
-	print("Unknown methods requested in hte config file.")
+    print("Unknown methods requested in hte config file.")
