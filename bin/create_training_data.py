1386d63f4da8efb82b938d03acfd2d3aa9d0a158
Adam R. Rivers <rivers@alum.mit.edu>
2015-07-23 18:18:36 -0700

    Added reftree parallel runtime parameters to config file and had Create_training_data.py use the config file for these

--- bin/create_training_data.py
+++ bin/create_training_data.py
@@ -13,10 +13,11 @@ args = parser.parse_args()
 
 
 #Variables
-numworkers = str(2)
 configpath = os.path.abspath(args.config)
 # Read the configuration file
-config = json.load( open(args.config, 'r'))
+config = json.load( open(configpath, 'r'))
+numworkers = str(config["numworkers"])
+resources = config["resources"]
 # If an id list is specified in config write it otherwise query everything from the root node \
 if config["trainingid"]:
 	node = str(config["trainingid"])
@@ -29,7 +30,7 @@ else:
 # script that calls single_taxon_training and writes the results to the reftree database directory
 #-db genomic --foreach 22 rt5 -- single_taxon_training.sh __TAXON__ __OUTFILE__ ~/dev/jgi-genelearn/bin/test/tstt.json
 
-reftreeopts = ["reftree.pl", "--db", "genomic","--slots",numworkers, "--resources", "-l ram.c=5G,h_rt=12:00:00", "--foreach", node, args.output,\
+reftreeopts = ["reftree.pl", "--db", "genomic","--slots",numworkers, "--resources", resources, "--foreach", node, args.output,\
 "--","/global/homes/a/arrivers/dev/jgi-genelearn/bin/single_taxon_training.sh", "__TAXON__","__OUTFILE__", configpath]
 #print(reftreeopts)
 p1 = subprocess.Popen(reftreeopts, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
