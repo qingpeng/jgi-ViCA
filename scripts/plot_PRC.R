80beb7f7228e9de35f9709c7e66ddc8cdcafcb54
Qingpeng Zhang <qingpeng@lbl.gov>
2016-05-05 19:48:45 -0700

    feature analysis

--- scripts/plot_PRC.R
+++ scripts/plot_PRC.R
@@ -14,10 +14,10 @@ PRC_PfamOnly = read_PRC("/global/projectb/scratch/qpzhang/Full_Training/Pfam/Pfa
 PRC_PfamVfam = read_PRC("/global/projectb/scratch/qpzhang/Full_Training/Pfam/Vfam_run/logistics_report/PRC/all.txt")
 
 ggplot() + 
-       geom_point(aes(V1, V2, colour = 'with Pfam,auPRC:0.9761'), data = PRC_withPfam_run2) + 
-       geom_point(aes(V1, V2, colour = 'No Pfam'), data = PRC_withoutPfam) + 
-       geom_point(aes(V1, V2, colour = 'Pfam Only, auPRC:0.9691'), data = PRC_PfamOnly) +
-       geom_point(aes(V1, V2, colour = 'with Pfam + Vfam, auPRC:0.9775'), data = PRC_PfamVfam) +
+       geom_point(aes(V1, V2, colour = 'with Pfam,auPRC:0.9761'), data = PRC_withPfam_run2,size=0.8) + 
+       geom_point(aes(V1, V2, colour = 'No Pfam'), data = PRC_withoutPfam,size=0.8) + 
+       geom_point(aes(V1, V2, colour = 'Pfam Only, auPRC:0.9691'), data = PRC_PfamOnly,size=0.8) +
+       geom_point(aes(V1, V2, colour = 'with Pfam + Vfam, auPRC:0.9775'), data = PRC_PfamVfam,size=0.8) +
        scale_colour_discrete("Features") +
-        xlab("Recall") + ylab("Precision")
+        xlab("Recall") + ylab("Precision") 
 
