c0b4f5a590ae2c51ac17780b5abfd2948931cc11
Qingpeng Zhang <qingpeng@lbl.gov>
2017-04-04 17:43:54 -0700

    add helper scripts for model tuning, get random subsampling

---
+++ extra/script/random_lines.sh
@@ -0,0 +1,4 @@
+python random_lines.py all_segment.fasta.vect.family.testing.svmlib.no4.virus 58000 5000 >virus_testing_5k.vect &
+python random_lines.py all_segment.fasta.vect.family.testing.svmlib.no4.non_virus 1579000 5000 >non_virus_testing_5k.vect &
+python random_lines.py all_segment.fasta.vect.family.training.svmlib.no4.virus 150600 25000 >virus_training_25k.vect &
+python random_lines.py all_segment.fasta.vect.family.training.svmlib.no4.non_virus 5076200 25000 >non_virus_training_25k.vect &
